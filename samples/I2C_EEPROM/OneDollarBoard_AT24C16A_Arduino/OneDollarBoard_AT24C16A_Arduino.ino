2862b967019fa0f573b9d153d08ec7eeb16ec570
Patrick Jackson <pjackson@noregon.com>
2024-08-19 19:23:43 -0400

    arduino EEPROM example update

--- samples/I2C_EEPROM/OneDollarBoard_AT24C16A_Arduino/OneDollarBoard_AT24C16A_Arduino.ino
+++ samples/I2C_EEPROM/OneDollarBoard_AT24C16A_Arduino/OneDollarBoard_AT24C16A_Arduino.ino
@@ -12,10 +12,19 @@ char data_array[16];
 
 void setup()
 {
-  memset( data, 0x42, sizeof( data ) );
+  byte temp;
+  memset( data_array, 0x42, sizeof( data_array ) );
 
   write_byte( 0x00, 'a' );
   write_page( 0x10, data_array );
+
+  temp = readEEPROM( 0x11 );
+
+  if ( temp == 0x42 )
+  {
+    pinMode( 0, OUTPUT );
+    digitalWrite( 0, HIGH );
+  }
 
 }
 
@@ -46,4 +55,21 @@ void write_byte( int addr, char data )
   TinyWireM.send( data );
   delay( 5 );
   TinyWireM.endTransmission();
+}
+
+byte readEEPROM( unsigned int eeaddress )
+{
+  byte rdata = 0xFF;
+  const uint8_t upper_three_eeprom_adress_bits = (eeaddress >> 8) & 0x7;
+  const uint8_t i2c_address = EEPROM_ADDR | upper_three_eeprom_adress_bits;
+  TinyWireM.beginTransmission(i2c_address);
+  TinyWireM.write(eeaddress & 0xFF); // address LSB
+  
+  TinyWireM.endTransmission();
+
+  TinyWireM.requestFrom(EEPROM_ADDR, 1);
+
+  if (TinyWireM.available()) rdata = TinyWireM.read();
+
+  return rdata;
 }
